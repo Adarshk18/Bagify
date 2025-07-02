0386d3ee9dd3abeec6d53aa4650de1ef8a27b8b4
Adarshk18 <adarshkohli183@gmail.com>
2025-07-02 21:26:11 +0530

    new

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const db = require("./config/mongoose-connection");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
 
 app.set("view engine","ejs");
 
@@ -10,6 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
+
+//acquiring routes
+app.use("/owners", ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products", productsRouter);
 
 app.get("/",(req,res)=>{
     res.send("Hi there");
