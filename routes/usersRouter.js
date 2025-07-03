dd0b2a75ab829188aba9100913cb6aa3e71f8ef2
Adarshk18 <adarshkohli183@gmail.com>
2025-07-03 20:53:27 +0530

    new

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     res.send("hii")
+});
+
+router.post("/register", async (req, res) => {
+    try {
+        let { email, fullname, password } = req.body;
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, async(err, hash) => {
+                if (err) return res.send(err.message);
+                else {
+                    let user = await userModel.create({
+                        email,
+                        fullname,
+                        password: hash,
+                    });
+                    
+                    let token = jwt.sign({email, id: user._id},"heyyyyy");
+                    res.cookie("token",token);
+                    res.send("user created successfully");
+                }
+            })
+        });
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 
